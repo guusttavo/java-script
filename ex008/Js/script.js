9c82ed95177f9cc49a94969de0aa5035eb5e5730
guusttavo <gustavoribeiroda112@gmail.com>
2024-09-09 22:28:34 -0300

    exercicio de js

--- ex008/Js/script.js
+++ ex008/Js/script.js
@@ -1,1 +1,21 @@
-alert('Olá!!')
+function carregar(){
+    var msg = window.document.getElementById ('msg')
+    var img = window.document.getElementById ('imagen')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12){
+        //Bom dia 
+         img.src = 'imagens/manha.png'
+         document.body.style.background = '#997b64'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde 
+         img.src = 'imagens/tarde.png'
+         document.body.style.background = '#f3831f'
+    } else {
+        //Boa noite
+         img.src = 'imagens/noite.png'
+         document.body.style.background = '#9f8479'
+    }
+}
+   
