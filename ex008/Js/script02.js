49601a10bcae3d6313e792282144225d30a5fff4
guusttavo <gustavoribeiroda112@gmail.com>
2024-09-18 20:53:13 -0300

    finalizado os exercicios

--- ex008/Js/script02.js
+++ ex008/Js/script02.js
@@ -9,12 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca-m.png')
+            } else if(idade < 21){
+                //Joven
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if(idade < 50){
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-m.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if(idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca-f.png')
+            } else if(idade < 21){
+                //Joven
+                img.setAttribute('src', 'imagens/jovem-f.png')
+            } else if(idade < 50){
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-f.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-f.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
