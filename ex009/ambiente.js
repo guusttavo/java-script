2357ddb009683bc670d15dd26b569bd27b4350b1
guusttavo <gustavoribeiroda112@gmail.com>
2024-09-18 22:07:20 -0300

    aula 13

---
+++ ex009/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++ 
+} while (c <= 6)
+
+
+
+
+
+// A mais usada dos comando de repetições
+
+/*var c = 1 
+while (c <= 6) {
+    console.log('Tudo bem')
+    c++ 
+}*/
+
+
+
+
+
+
+//console.log('Tudo bem')
