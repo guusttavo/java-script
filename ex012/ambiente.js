b6bfec9713d9016f7cba2a910e9217b1865f554e
guusttavo <gustavoribeiroda112@gmail.com>
2024-10-28 20:52:51 -0300

    atualização da aula 14

--- ex012/ambiente.js
+++ ex012/ambiente.js
@@ -1,3 +1,12 @@
-let num = [5,6,7,8,9,10]
-
-console.log(`Nosso vetor é o ${num}`)
+let num = [5,7,8,9,10]
+num.sort()
+num.push(7)
+console.log(num)
+console.log(`O vetor tem ${num.length} posiçoões`)
+console.log(`Nosso vetor é o ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1){
+    console.log('O valor não foi encontrado!')
+}else {
+    console.log(`o valor está na posição ${pos}`)
+}
